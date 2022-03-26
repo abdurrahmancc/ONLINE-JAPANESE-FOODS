ec887f23525f5dac8444a907bea2b2c3607f1e9e
abdurrahmancc <personalbusinessmail63@gmail.com>
2022-03-26 13:04:00 +0600

    again added

--- src/Components/AllProduct/AllProduct.js
+++ src/Components/AllProduct/AllProduct.js
@@ -4,38 +4,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import Product from "../Products/Product";
 import Selected from "../Selected/Selected";
+import "./AllProduct.css";
 
 const AllProduct = () => {
   const [products, setProducts] = useState([]);
   const [product, setproduct] = useState([]);
   const [randomNumbers, setRandomNumbers] = useState(0);
+  const [disply, setDisplay] = useState("");
   useEffect(() => {
     fetch("data.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
   console.log(product);
-  console.log(product.length);
-
+  console.log(disply);
   //   selectedItems
   const selectedItems = (props) => {
     // const search = product.find((item) => item == props);
     // console.log(search);
-    if (props != product.find((item) => item == props)) {
-      const newproduct = [...product, props];
-      setproduct(newproduct);
-      //   console.log(newproduct);
+    if (product.length <= 3) {
+      if (props != product.find((item) => item == props)) {
+        const newproduct = [...product, props];
+        setproduct(newproduct);
+        //   console.log(newproduct);
+      } else {
+        alert("all ready added");
+      }
     } else {
-      alert("all ready added");
+      alert("");
     }
   };
 
   //   random Number create
   const randomNumber = () => {
-    const number = product[Math.floor(Math.random() * product.length)];
-    setRandomNumbers(number);
+    if (product.length >= 1) {
+      const number = product[Math.floor(Math.random() * product.length)];
+      setRandomNumbers(number);
+    } else {
+      alert("You have no selected items");
+    }
   };
-  console.log(randomNumbers);
+  console.log(product.length);
+
+  // choose again section
+  const chooseAgain = () => {
+    const empty = [];
+    setproduct(empty);
+    setRandomNumbers(empty);
+  };
   return (
     <div className="container">
       <h1 className="text-primary mb-5">ONLINE JAPANESE FOODS</h1>
@@ -53,10 +69,10 @@ const AllProduct = () => {
         {/* -----------------------------
               selected section
         ------------------------------ */}
-        <div className="col-md-4 col-sm-12 order-first order-md-last order-lg-last">
+        <div className="col-md-4 col-sm-12  order-first order-md-last order-lg-last">
           <div
             style={{ background: "#DCDCDC", height: "100vh", position: "sticky", top: "0px" }}
-            className=" pt-5"
+            className="rounded rounded-3 pt-2"
           >
             <h4 className="text-center">Selected Items</h4>
             <div>
@@ -68,22 +84,14 @@ const AllProduct = () => {
               <button onClick={() => randomNumber()} className="btn btn-info p-1 me-2">
                 choose 1 for me
               </button>
-              <button className="btn btn-info  mt-md-2 mt-lg-0 p-1">choose again</button>
+              <button onClick={chooseAgain} className="btn btn-info  mt-md-2 mt-lg-0 p-1">
+                choose again
+              </button>
               <div>
-                <div className="text-start mx-4 p-3 d-flex justify-content-between">
-                  <div className="card shadow-sm p-2">
+                <div className="text-start mx-4 p-3 d-flex justify-content-between ">
+                  <div className="card  shadow-sm ">
                     <img src={randomNumbers.img} alt="" />
-                    <h6 className="text-start pt-3">{randomNumbers.name}</h6>
-                    <h6 className="text-start">price: {randomNumbers.price}</h6>
-                    <div className="text-end">
-                      <button onClick={() => selectedItems(product)} className="btn btn-primary ">
-                        Add to cart
-                        <FontAwesomeIcon
-                          className="ms-2 fa-sm"
-                          icon={faShoppingCart}
-                        ></FontAwesomeIcon>
-                      </button>
-                    </div>
+                    <h6 className=" text-start pt-3 ">{randomNumbers.name}</h6>
                   </div>
                 </div>
               </div>
